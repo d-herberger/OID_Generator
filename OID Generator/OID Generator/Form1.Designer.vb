ed4183b7b8fedd4bb03e3219e2f12b1d6d9f6846
Dennis Herberger <20538070+d-herberger@users.noreply.github.com>
2017-10-12 10:27:51 -0500

    Initial Upload

---
+++ OID Generator/OID Generator/Form1.Designer.vb
@@ -0,0 +1,91 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.btnGenerate = New System.Windows.Forms.Button()
+        Me.txtRootOID = New System.Windows.Forms.TextBox()
+        Me.lblRoot = New System.Windows.Forms.Label()
+        Me.lblDescription = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnGenerate
+        '
+        Me.btnGenerate.Location = New System.Drawing.Point(229, 129)
+        Me.btnGenerate.Name = "btnGenerate"
+        Me.btnGenerate.Size = New System.Drawing.Size(114, 23)
+        Me.btnGenerate.TabIndex = 0
+        Me.btnGenerate.Text = "Generate OID"
+        Me.btnGenerate.UseVisualStyleBackColor = True
+        '
+        'txtRootOID
+        '
+        Me.txtRootOID.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.txtRootOID.Location = New System.Drawing.Point(12, 103)
+        Me.txtRootOID.Name = "txtRootOID"
+        Me.txtRootOID.ReadOnly = True
+        Me.txtRootOID.Size = New System.Drawing.Size(520, 13)
+        Me.txtRootOID.TabIndex = 1
+        '
+        'lblRoot
+        '
+        Me.lblRoot.AutoSize = True
+        Me.lblRoot.Location = New System.Drawing.Point(13, 84)
+        Me.lblRoot.Name = "lblRoot"
+        Me.lblRoot.Size = New System.Drawing.Size(85, 13)
+        Me.lblRoot.TabIndex = 2
+        Me.lblRoot.Text = "Your root OID is:"
+        '
+        'lblDescription
+        '
+        Me.lblDescription.AutoSize = True
+        Me.lblDescription.Location = New System.Drawing.Point(13, 13)
+        Me.lblDescription.Name = "lblDescription"
+        Me.lblDescription.Size = New System.Drawing.Size(542, 52)
+        Me.lblDescription.TabIndex = 3
+        Me.lblDescription.Text = resources.GetString("lblDescription.Text")
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(569, 179)
+        Me.Controls.Add(Me.lblDescription)
+        Me.Controls.Add(Me.lblRoot)
+        Me.Controls.Add(Me.txtRootOID)
+        Me.Controls.Add(Me.btnGenerate)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "OID Generator for Active Directory Custom Attributes"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnGenerate As System.Windows.Forms.Button
+    Friend WithEvents txtRootOID As System.Windows.Forms.TextBox
+    Friend WithEvents lblRoot As System.Windows.Forms.Label
+    Friend WithEvents lblDescription As System.Windows.Forms.Label
+
+End Class
---
+++ OID Generator/OID Generator/Form1.vb
@@ -0,0 +1,46 @@
+﻿Public Class Form1
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub btnGenerate_Click(sender As Object, e As EventArgs) Handles btnGenerate.Click
+        'Initializing Variables
+        Dim guidString, oidPrefix
+        Dim guidPart0, guidPart1, guidPart2, guidPart3, guidPart4, guidPart5, guidPart6
+        Dim oidPart0, oidPart1, oidPart2, oidPart3, oidPart4, oidPart5, oidPart6
+        On Error Resume Next
+        'Generate GUID
+        Dim TypeLib = CreateObject("Scriptlet.TypeLib")
+        guidString = TypeLib.Guid
+        'If no network card is available on the machine then generating GUID can result with an error.
+
+        If Err.Number <> 0 Then
+            txtRootOID.Text = "ERROR:Guid could not be generated, please ensure machine has a network card."
+        End If
+        'Stop Error Resume Next
+        On Error GoTo 0
+        'The Microsoft OID Prefix used for the automated OID Generator
+        oidPrefix = "1.2.840.113556.1.8000.2554"
+        'Split GUID into 6 hexadecimal numbers
+        guidPart0 = Trim(Mid(guidString, 2, 4))
+        guidPart1 = Trim(Mid(guidString, 6, 4))
+        guidPart2 = Trim(Mid(guidString, 11, 4))
+        guidPart3 = Trim(Mid(guidString, 16, 4))
+        guidPart4 = Trim(Mid(guidString, 21, 4))
+        guidPart5 = Trim(Mid(guidString, 26, 6))
+        guidPart6 = Trim(Mid(guidString, 32, 6))
+        'Convert the hexadecimal to decimal
+        oidPart0 = CLng("&H" & guidPart0)
+        oidPart1 = CLng("&H" & guidPart1)
+        oidPart2 = CLng("&H" & guidPart2)
+        oidPart3 = CLng("&H" & guidPart3)
+        oidPart4 = CLng("&H" & guidPart4)
+        oidPart5 = CLng("&H" & guidPart5)
+        oidPart6 = CLng("&H" & guidPart6)
+        'Concatenate all the generated OIDs together with the assigned Microsoft prefix and return
+        Dim GenerateOID = oidPrefix & "." & oidPart0 & "." & oidPart1 & "." & oidPart2 & "." & oidPart3 & _
+            "." & oidPart4 & "." & oidPart5 & "." & oidPart6
+        txtRootOID.Text = GenerateOID
+    End Sub
+End Class
